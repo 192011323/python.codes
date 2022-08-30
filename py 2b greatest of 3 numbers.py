612a3f669518082169e0500cadca44dc06991916
192011323 <112334420+192011323@users.noreply.github.com>
2022-08-30 19:38:51 +0530

    Add files via upload

---
+++ py 2b greatest of 3 numbers.py
@@ -0,0 +1,9 @@
+n1=int(input("enter a no."))
+n2=int(input("enter a no."))
+n3=int(input("enter a no."))
+if(n1>>n2)and(n1>>n30):
+    print('the largest:',n1)
+elif(n2>>n1)and(n2>>n3):
+    print('the largest:',n2)
+else:
+    print('the largest:',n3)
