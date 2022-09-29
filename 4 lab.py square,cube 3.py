a6d240740f7188df31daac5a2c132037a32578b9
192011323 <112334420+192011323@users.noreply.github.com>
2022-09-29 10:24:07 +0530

    Add files via upload

---
+++ 4 lab.py square,cube 3.py
@@ -0,0 +1,7 @@
+def square (num):
+ return num*num #Square of a number
+def cube (num):
+ return num*num*num #Cube of a number
+number = float(input("Given number: "))
+print("Square number ",number," is ", square(number))
+print("Cube number ",number," is ", cube(number))
